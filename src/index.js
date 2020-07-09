5c908da43c4751b027b107363786a40b86816e11
Ben Douthwaite <Ben.L.Douthwaite@gmail.com>
2020-07-09 19:07:52 +0100

    Adding simple landing page from serverless stack tut and configure aws amplify

--- src/index.js
+++ src/index.js
@@ -2,10 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { Amplify } from 'aws-amplify';
+
+Amplify.configure({
+    // Add s3 buckets or lambdas here when needed
+});
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+      <Router>
+          <App />
+      </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
