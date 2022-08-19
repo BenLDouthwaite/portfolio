e4b8a1789a80c3cfd9375b5d8cdd923762a11813
Ben Douthwaite <Ben.L.Douthwaite@gmail.com>
2022-08-19 15:45:21 +0100

    Add dummy call to API to test API and DB connection

--- src/App.js
+++ src/App.js
@@ -1,13 +1,35 @@
+import React, { useEffect, useState } from "react"
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [tasks, setTasks] = useState([]);
+  
+  useEffect(() => {
+    fetch("https://evening-fjord-54109.herokuapp.com/db")
+      .then(res => res.json())
+      .then(
+        (result) => {
+          console.log(result)
+          // setIsLoaded(true);
+          setTasks(result);
+        },
+        // Note: it's important to handle errors here
+        // instead of a catch() block so that we don't swallow
+        // exceptions from actual bugs in components.
+        (error) => {
+          // setIsLoaded(true);
+          // setError(error);
+        }
+      )
+  }, [])
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save to reload
         </p>
         <a
           className="App-link"
@@ -17,6 +39,14 @@ function App() {
         >
           Learn React
         </a>
+        <h1>Test API Call - Read from DB</h1>
+        <ul>
+          {tasks.map(task => (
+            <li key={task}>
+              {task}
+            </li>
+          ))}
+        </ul>
       </header>
     </div>
   );
