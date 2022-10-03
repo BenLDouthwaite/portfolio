cf3d20e65c2f42632d4b81317403b248d6c58ffb
Ben Douthwaite <Ben.L.Douthwaite@gmail.com>
2022-10-03 23:12:48 +0100

    Initial commit, referencing Callicoder's Spring React Oauth tutorial, needs reducing down to simplest form

--- src/App.js
+++ src/App.js
@@ -1,53 +1,28 @@
-import React, { useEffect, useState } from "react"
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter } from "react-router-dom";
+import { Route, Routes } from "react-router";
+
+import Home from "./pages/home";
+import Db from "./pages/db";
+import Tasks from "./pages/tasks";
+import Login from "./pages/login";
+import OAuth2RedirectHandler from "./user/oauth2/OAuth2RedirectHandler";
+
+import "./App.css";
 
 function App() {
-  const [tasks, setTasks] = useState([]);
-  
-  useEffect(() => {
-    fetch("https://evening-fjord-54109.herokuapp.com/db")
-      .then(res => res.json())
-      .then(
-        (result) => {
-          console.log(result)
-          // setIsLoaded(true);
-          setTasks(result);
-        },
-        // Note: it's important to handle errors here
-        // instead of a catch() block so that we don't swallow
-        // exceptions from actual bugs in components.
-        (error) => {
-          // setIsLoaded(true);
-          // setError(error);
-        }
-      )
-  }, [])
-
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <h1>Test API Call - Read from DB</h1>
-        <ul>
-          {tasks.map(task => (
-            <li key={task}>
-              {task}
-            </li>
-          ))}
-        </ul>
-      </header>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/db" element={<Db />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/tasks" element={<Tasks />} />
+          <Route path="/oauth2/redirect" element={<OAuth2RedirectHandler />} />
+          {/* <Route path="/oauth2/redirect" element={<Navigate to="/" replace/>} /> */}
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
